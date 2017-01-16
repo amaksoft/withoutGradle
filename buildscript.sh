ea54357fdb24f094ad777cef9ec5d91fdae0007a
Andrey Makeev <andrey.makeev@altarix.ru>
2017-01-16 17:29:41 +0400

    Parametrise script more, add appcompat

--- buildscript.sh
+++ buildscript.sh
@@ -9,6 +9,23 @@ COMPILE_SDK_VERSION="25"
 BUILD_TOOLS_VERSION="25.0.1"
 PACKAGE_NAME="ru.altarix.training.withoutgradle"
 
+V_APP_COMPAT="25.1.0"
+
+# Локальный maven-репозиторий, устанавливаемый с SDK
+LOCAL_REPO="${ANDROID_HOME}/extras/android/m2repository"
+
+# Библиотеки:
+LIBRARIES="${LOCAL_REPO}/com/android/support/appcompat-v7/${V_APP_COMPAT}/appcompat-v7-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-v4/${V_APP_COMPAT}/support-v4-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/animated-vector-drawable/${V_APP_COMPAT}/animated-vector-drawable-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-annotations/${V_APP_COMPAT}/support-annotations-${V_APP_COMPAT}.jar
+        ${LOCAL_REPO}/com/android/support/support-compat/${V_APP_COMPAT}/support-compat-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-core-ui/${V_APP_COMPAT}/support-core-ui-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-core-utils/${V_APP_COMPAT}/support-core-utils-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-fragment/${V_APP_COMPAT}/support-fragment-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-media-compat/${V_APP_COMPAT}/support-media-compat-${V_APP_COMPAT}.aar
+        ${LOCAL_REPO}/com/android/support/support-vector-drawable/${V_APP_COMPAT}/support-vector-drawable-${V_APP_COMPAT}.aar"
+
 # ======= Создаем вспомогательные переменные директории =======
 # Скрипт должен находиться в корне проекта, записываем в переменную среды как директорию корневого проекта
 ROOT_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
@@ -20,6 +37,21 @@ APP_PROJECT_DIR="${ROOT_PROJECT_DIR}/app"
 APP_BUILD_DIR="$APP_PROJECT_DIR/build"
 # Создаем директорию, если ее нет.
 mkdir -p ${APP_BUILD_DIR}
+
+# ======= Распаковываем либы =======
+echo "=== Preparing libs..."
+for LIB in ${LIBRARIES}; do
+    LIBDIR="${APP_BUILD_DIR}/intermediates_/exploded-aar/${LIB##*/}"
+    EX_LIBS+="${LIBDIR} "
+    mkdir -p ${LIBDIR}
+    unzip -q ${LIB} -d ${LIBDIR}
+    if [[ -f ${LIBDIR}/classes.jar ]]; then
+        LIBS_JARS+="${LIBDIR}/classes.jar "
+    fi
+    if [ "$(ls -A ${LIBDIR}/res 2> /dev/null)" ]; then
+        LIBS_RES_AAPT+="-S ${LIBDIR}/res "
+    fi
+done
 
 # ======= Создаем ключ для подписи apk =======
 echo "=== (Re)Making keystore..."
@@ -49,10 +81,13 @@ ${ANDROID_HOME}/build-tools/${BUILD_TOOLS_VERSION}/aapt \
         package \
         -f \
         -m \
+        --auto-add-overlay \
         -S ${APP_PROJECT_DIR}/src/main/res \
         -J ${GEN_SOURCES_DIR} \
         -M ${APP_PROJECT_DIR}/src/main/AndroidManifest.xml \
-        -I ${ANDROID_HOME}/platforms/android-${COMPILE_SDK_VERSION}/android.jar
+        -I ${ANDROID_HOME}/platforms/android-${COMPILE_SDK_VERSION}/android.jar \
+        --extra-packages android.support.v7.appcompat:android.support.v4 \
+        ${LIBS_RES_AAPT}
 
 # ======= Компилируем .class файлы =======
 echo "=== Compiling with javac..."
@@ -65,10 +100,12 @@ ${JAVA_HOME}/bin/javac \
         -d ${CLASSES_DIR} \
         -g \
         -encoding UTF-8 \
-        -bootclasspath /home/amak/Android/Sdk/platforms/android-${COMPILE_SDK_VERSION}/android.jar \
+        -bootclasspath /home/amak/Android/Sdk/platforms/android-25/android.jar \
         -sourcepath ${APP_SOURCES_DIR} \
-        -classpath ${CLASSES_DIR} \
+        -classpath ${LIBS_JARS// /:} \
             ${GEN_SOURCES_DIR}/${PACKAGE_NAME//./\/}/R.java \
+            ${GEN_SOURCES_DIR}/android/support/v4/R.java \
+            ${GEN_SOURCES_DIR}/android/support/v7/appcompat/R.java \
             ${APP_SOURCES_DIR}/${PACKAGE_NAME//./\/}/MainActivity.java \
         -XDuseUnsharedTable=true
 
@@ -81,6 +118,7 @@ ${ANDROID_HOME}/build-tools/${BUILD_TOOLS_VERSION}/dx \
         --dex \
         --output=${DEX_DIR}/classes.dex \
         ${CLASSES_DIR} \
+        ${LIBS_JARS}
 
 # ======= Собираем apk =======
 echo "=== Producing APK..."
@@ -90,14 +128,16 @@ mkdir -p ${APK_INTERMEDIATES}
 ${ANDROID_HOME}/build-tools/${BUILD_TOOLS_VERSION}/aapt \
         package \
         -f \
-        --auto-add-overlay \
         --min-sdk-version 9 \
         --target-sdk-version 25 \
         --version-code 1 \
         --version-name 1.0 \
+        --auto-add-overlay \
         -S ${APP_PROJECT_DIR}/src/main/res \
         -M ${APP_PROJECT_DIR}/src/main/AndroidManifest.xml \
         -I ${ANDROID_HOME}/platforms/android-${COMPILE_SDK_VERSION}/android.jar \
+        --extra-packages android.support.v7.appcompat:android.support.v4 \
+        ${LIBS_RES_AAPT} \
         -F ${APK_INTERMEDIATES}/${PROJECT_NAME}.unsigned.apk \
         ${DEX_DIR}
 
